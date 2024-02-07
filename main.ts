415a3e85a0c19da94c4a2f569ce5fd9cf474e516
J3BBR3YB4ZZ0S <159304906+J3BBR3YB4ZZ0S@users.noreply.github.com>
2024-02-07 08:56:16 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+let Happy_Pet = images.createImage(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+let Sad_Pet = images.createImage(`
+    . . . . .
+    . # . # .
+    . . . . .
+    . # # # .
+    # . . . #
+    `)
+let Dead_Pet = images.createImage(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
